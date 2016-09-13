e77b8ab2030cd5a214d02aada9a0cb4293164b71
Allyperlman <PerlmanA7338@sduhsd.lan>
2016-09-13 11:49:59 -0700

    the real lesson 2

---
+++ APLESSON_03/Lesson_02_fixed.java
@@ -0,0 +1,20 @@
+public class Lesson_02_fixed
+{
+	public static void main(String[]args)
+	{
+		int x =2;
+		int y =4;
+		int product = x * y;
+		System.out.println("2 multiplied by 4 is " +" "+ product);
+		
+		System.out.println("Ally Perlman");
+		System.out.println("15135 Paso Del Sol");
+		System.out.println("Del Mar, CA 92014");
+		
+		int w = 2;
+		int l = 3;
+		int h = 4;
+		int RectangularArea = w*l+h*l+h*w+w*l+h*l+h*w;
+		System.out.println("The surface area of your cube is "+ RectangularArea);
+	}
+}
