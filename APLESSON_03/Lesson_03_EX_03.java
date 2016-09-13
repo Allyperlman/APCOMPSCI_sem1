0e8419fb7d9a1b2e6b298b2131ee6304d9ecdf3b
Allyperlman <PerlmanA7338@sduhsd.lan>
2016-09-13 12:10:16 -0700

    updated gitignore added Lesson_03_EX_03

---
+++ APLESSON_03/Lesson_03_EX_03.java
@@ -0,0 +1,12 @@
+public class Lesson_03_EX_03
+{
+	public static void main(String[]args)
+	{
+		int x =111;
+		int y =222;
+		int product = x * y;
+		System.out.println("111 multiplied by 222 is "+ product);
+		
+	}
+		
+}
