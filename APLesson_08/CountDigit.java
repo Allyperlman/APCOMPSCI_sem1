d0630cd46e46a61910edc7bc56703ad5e4a612de
Allyperlman <PerlmanA7338@sduhsd.lan>
2016-11-15 12:07:54 -0800

    Lesson_08

---
+++ APLesson_08/CountDigit.java
@@ -0,0 +1,21 @@
+import java.util.Scanner; 
+public class CountDigit
+{
+	static String sentence;
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter a sentence");
+		System.out.println(replace(kb.nextLine()));
+	}
+	
+	public static String replace(String sentence)
+	{
+		if(sentence.indexOf(" ") > 0)
+			 return replace(sentence.substring(0, sentence.indexOf(" ")) +"_"+ sentence.substring(sentence.indexOf(" ")+1));
+		else
+		{
+			return sentence;
+		}
+	}
+}
