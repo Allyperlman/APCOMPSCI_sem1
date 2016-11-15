55713ba451b3670831ca3ce9279268f516b78080
Allyperlman <PerlmanA7338@sduhsd.lan>
2016-11-15 10:36:29 -0800

    fixed lesson_04

--- APLESSON_04/CompoundInterest.java
+++ APLESSON_04/CompoundInterest.java
@@ -1,12 +1,18 @@
+import java.util.Scanner; 
 public class CompoundInterest
 {
 	public static void main(String[]args)
 	{
+		Scanner kb = new Scanner(System.in);
 		CompoundInterest average = new CompoundInterest();
-		double r = 3;
-		double P = 40;
-		double n = 2;
-		double t = 5;
+		System.out.println("Please enter ");
+		double r = kb.nextDouble();
+		System.out.println("Please enter ");
+		double P = kb.nextDouble();
+		System.out.println("Please enter ");
+		double n = kb.nextDouble();
+		System.out.println("Please enter ");
+		double t = kb.nextDouble();
 		double comp = average.calcInterest(r, P, n,t);
 		System.out.printf("Your total monthly payment is $%3.2f\n",comp);
 		
@@ -14,6 +20,6 @@ public class CompoundInterest
 	
 	public double calcInterest(double r, double P, double n, double t)
 	{
-		return((P*(Math.pow( 1+r/n, n*t))))(t*12);
+		return((P*(Math.pow(1+r/n, n*t))))/(t*12);
 	}
 }
