92a51114a3c6f6cb43fbd6e3d6ba7c6877c235ad
Allyperlman <PerlmanA7338@sduhsd.lan>
2016-11-15 10:58:53 -0800

    updated

--- APLESSON_04/CompoundInterest.java
+++ APLESSON_04/CompoundInterest.java
@@ -3,8 +3,8 @@ public class CompoundInterest
 {
 	public static void main(String[]args)
 	{
-		Scanner kb = new Scanner(System.in);
 		CompoundInterest average = new CompoundInterest();
+		Scanner kb = new Scanner(System.in);
 		System.out.println("Please enter the interest rate ");
 		double r = kb.nextDouble();
 		System.out.println("Please enter the principal");
@@ -13,6 +13,7 @@ public class CompoundInterest
 		double n = kb.nextDouble();
 		System.out.println("Please enter the life of the loan in years");
 		double t = kb.nextDouble();
+
 		double comp = average.calcInterest(r, P, n,t);
 		System.out.printf("Your total monthly payment is $%3.2f\n",comp);
 		
@@ -20,6 +21,6 @@ public class CompoundInterest
 	
 	public double calcInterest(double r, double P, double n, double t)
 	{
-		return((P*(Math.pow(1+r/n, n*t))))/(t*12);
+		return P*(Math.pow(1+r/n, n*t))/(t*12);
 	}
 }
