f034873a3a43feee1a074eb1771fce130028ebe7
Allyperlman <PerlmanA7338@sduhsd.lan>
2016-11-15 10:42:52 -0800

    REAL fixed Lesson_04

--- APLESSON_04/CompoundInterest.java
+++ APLESSON_04/CompoundInterest.java
@@ -5,13 +5,13 @@ public class CompoundInterest
 	{
 		Scanner kb = new Scanner(System.in);
 		CompoundInterest average = new CompoundInterest();
-		System.out.println("Please enter ");
+		System.out.println("Please enter the interest rate ");
 		double r = kb.nextDouble();
-		System.out.println("Please enter ");
+		System.out.println("Please enter the principal");
 		double P = kb.nextDouble();
-		System.out.println("Please enter ");
+		System.out.println("Please enter the number of times the loan is compounded per year");
 		double n = kb.nextDouble();
-		System.out.println("Please enter ");
+		System.out.println("Please enter the life of the loan in years");
 		double t = kb.nextDouble();
 		double comp = average.calcInterest(r, P, n,t);
 		System.out.printf("Your total monthly payment is $%3.2f\n",comp);
